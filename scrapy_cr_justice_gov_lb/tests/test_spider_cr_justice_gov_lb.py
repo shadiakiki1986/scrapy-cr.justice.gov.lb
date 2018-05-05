f78cfe50d6edea1f499d73bc47a131b4f32a27dc
Shadi Akiki <shadiakiki1986@gmail.com>
2018-05-05 06:21:31 +0000

    add test for spider after_search .. still needs fixing

--- scrapy_cr_justice_gov_lb/tests/test_spider_cr_justice_gov_lb.py
+++ scrapy_cr_justice_gov_lb/tests/test_spider_cr_justice_gov_lb.py
@@ -12,7 +12,7 @@ from ..spiders.cr_justice_gov_lb import ScrapyCrJusticeGovLbSpiderCsv
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-from .update_fixtures import CASSETTE_LIBRARY_DIR
+CASSETTE_LIBRARY_DIR = os.path.join(BASE_DIR, 'tests/fixtures/cassettes/')
 with Betamax.configure() as config:
   config.cassette_library_dir = CASSETTE_LIBRARY_DIR
 
@@ -24,13 +24,37 @@ class TestSpiderCrJusticeGovLb(BetamaxTestCase):
     self.spider = ScrapyCrJusticeGovLbSpiderCsv(df_in)
     super().setUp()
 
-  def test_parse(self):
+  def test_1_parse(self):
+    response = self.session.get(self.spider.url)
+    response = list(self.spider.parse(response))
+    self.assertEqual(1, len(response))
+    response = response[0]
+    self.assertEqual('POST', response.method)
+    self.assertTrue('FindBox=66942' in str(response.body))
+
+  def test_2_after_search(self):
     response = self.session.get(self.spider.url)
     request = list(self.spider.parse(response))
-    self.assertEqual(1, len(request))
     request = request[0]
-    self.assertEqual('POST', request.method)
-    self.assertTrue('FindBox=66942' in str(request._body))
+    # print(request.__dict__)
+
+    # method 1: plain requests post
+    response = self.session.post(request.url, data=str(request.body))
+
+    # method 2
+    # copied from scrapy.shell#L114
+    # Blocks and never returns .. FIXME
+#    from scrapy.utils.test import get_crawler
+#    from scrapy.shell import Shell
+#    crawler = get_crawler()
+#    shell = Shell(crawler)
+#    response = shell.fetch(request, spider=self.spider, redirect=False)
+
+    response = list(self.spider.after_search(response))
+    self.assertEqual(1, len(response))
+    response = response[0]
+    self.assertEqual('GET', response.method)
+    self.assertTrue('FindBox=66942' in str(response._body))
 
 
 if __name__ == '__main__':
