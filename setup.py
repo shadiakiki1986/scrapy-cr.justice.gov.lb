64bad2ccd13302cfe0fcc5f6a5ff9870831a4db4
Shadi Akiki <shadiakiki1986@gmail.com>
2018-05-10 13:03:40 +0300

    add setup.py to be able to install with pip

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='scrapy_cr_justice_gov_lb',
+    packages=find_packages(),
+    version='0.0.1',
+    description='scrapy spider for cr.justice.gov.lb',
+    author='Shadi Akiki',
+    license='BSD-3',
+)
