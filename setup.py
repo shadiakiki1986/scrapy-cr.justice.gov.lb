6e121f27a42c932a497f28fe8cf82560e9fb8aa2
Shadi Akiki <shadiakiki1986@gmail.com>
2018-06-28 16:18:42 +0300

    version bump in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='scrapy_cr_justice_gov_lb',
     packages=find_packages(),
-    version='0.1.1',
+    version='0.1.2',
     description='scrapy spider for cr.justice.gov.lb',
     author='Shadi Akiki',
     license='BSD-3',
